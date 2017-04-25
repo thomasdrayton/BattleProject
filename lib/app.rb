650c4f77c389a4eb31340b842d49de8d78fec8eb
Thomas Drayton <tomadrayton@gmail.com>
2017-04-25 16:34:16 +0100

    FIGHT

--- lib/app.rb
+++ lib/app.rb
@@ -6,8 +6,14 @@ class Battle < Sinatra::Base
     "Testing infrastructure working!"
   end
 
-  get '/begin' do
+  get '/names' do
     erb(:nameform)
+  end
+
+  post '/fight' do
+    #@name1 = params[:name1]
+    #@name2 = params[:name2]
+    erb(:namesgiven)
   end
 
 # start the server if ruby file executed directly
